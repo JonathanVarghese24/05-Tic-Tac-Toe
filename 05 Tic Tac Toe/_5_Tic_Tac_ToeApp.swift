47450522cee08107ee085973b7beb6377ba99e83
Jonathan Varghese <jonathanvarghese24@gmail.com>
2023-11-14 09:29:57 -0600

    Initial Commit

---
+++ 05 Tic Tac Toe/_5_Tic_Tac_ToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  _5_Tic_Tac_ToeApp.swift
+//  05 Tic Tac Toe
+//
+//  Created by Jonathan Varghese on 11/14/23.
+//
+
+import SwiftUI
+
+@main
+struct _5_Tic_Tac_ToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
