8d3b65778d2f77f60383af85651ddaf8ac9be9d1
Jonathan Varghese <jonathanvarghese24@gmail.com>
2023-11-16 09:56:00 -0600

    Added text to cell tiles

--- 05 Tic Tac Toe/ContentView.swift
+++ 05 Tic Tac Toe/ContentView.swift
@@ -10,17 +10,31 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            
+            Text("Tic Tac Toe")
+                .font(.title).bold()
+            
+            LazyVGrid(columns: Array(repeating: GridItem(.fixed(120), spacing: 15), count: 3), spacing: 15) {
+                
+                ForEach(0..<9) { index in
+                    ZStack{
+                        Color.blue
+                        Text("X")
+                            .font(.system(size: 90))
+                            .fontWeight(.heavy)
+                    }
+                    .frame(width: 120, height: 120, alignment: .center)
+                    .cornerRadius(30)
+                }
+            }
+            .preferredColorScheme(.dark)
+            .padding()
         }
-        .padding()
     }
 }
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
+    }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
